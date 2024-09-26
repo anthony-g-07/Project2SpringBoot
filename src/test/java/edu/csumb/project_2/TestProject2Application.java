be79e5bee2b712fdd9e39dfb37966e8b8438885c
anthonyg-07 <126286102+anthony-g-07@users.noreply.github.com>
2024-09-26 07:47:50 -0700

    Created project

---
+++ src/test/java/edu/csumb/project_2/TestProject2Application.java
@@ -0,0 +1,11 @@
+package edu.csumb.project_2;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.from(Project2Application::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
