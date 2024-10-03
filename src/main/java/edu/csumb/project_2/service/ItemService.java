abbfd9e1840cdc7f9437b47e56af0003cab517d1
anthonyg-07 <126286102+anthony-g-07@users.noreply.github.com>
2024-10-02 17:12:13 -0700

    Created add, delete function for items

---
+++ src/main/java/edu/csumb/project_2/service/ItemService.java
@@ -0,0 +1,45 @@
+package edu.csumb.project_2.service;
+
+import edu.csumb.project_2.model.Item;
+import edu.csumb.project_2.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public List<Item> getItemByUser(String userId, String listId) {
+        return itemRepository.findByUserIdAndListId(userId, listId);
+    }
+
+    public List<Item> getItemsByListId(String listId) {
+        return itemRepository.findByListId(listId);
+    }
+
+    public Optional<Item> getItemById(String itemId) {
+        return itemRepository.findById(itemId);
+    }
+
+    public Item addItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+    public boolean deleteItem(String itemNameOrId) {
+        Item item = (Item) itemRepository.findByName(itemNameOrId);
+        if (item != null) {
+            itemRepository.delete(item);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+}
